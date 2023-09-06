1bcbf86b81869ece64dedd8090c0ec549fc4a665
Reyhaneh H.M <reyhaneh3784@gmail.com>
2023-09-06 15:18:15 +0330

    start 1

---
+++ script/script2.js
@@ -0,0 +1,11 @@
+const showMenu = document.querySelector('#show-menu')
+    showMenu.onclick = function () {
+        //document.getElementById("box-mother-menu").classList.replace("box-mother-menu-close","box-mother-menu-open")
+        document.getElementById("box-mother-menu").classList.add('box-mother-menu-close')
+}
+
+const removeMenu = document.querySelector('#remove-menu')
+    removeMenu.onclick = function () {
+        document.getElementById("box-mother-menu").classList.replace("box-mother-menu-open","box-mother-menu-close")
+}
+console.log()
