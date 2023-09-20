730347934f59da26163da2a4f9991ded939b1e8d
Reyhaneh H.M <reyhaneh3784@gmail.com>
2023-09-20 17:56:09 +0330

    Add following menu to project

--- script/script2.js
+++ script/script2.js
@@ -9,3 +9,28 @@ const removeMenu = document.querySelector('#remove-menu')
         document.getElementById("box-mother-menu").classList.remove("box-mother-menu-close")
 }
 console.log()
+
+$('#filter a').click(function () {
+    // alert('salam')
+    let className = $(this).attr('class')
+
+    // console.log(className)
+    $('a.active').removeClass('active');
+    $(this).addClass('active')
+
+    if (className === 'all') {
+        $('.item').fadeIn(500)
+    }else{
+        $('.item').hide().filter('.'+className).fadeIn(500)
+
+    }
+    return false;
+})
+
+
+$('#top-menu li').hover(function () {
+    $(this).children('ul').stop(true).show('slow')
+},function () {
+    $(this).children('ul').stop(true).hide('slow')
+})
+
